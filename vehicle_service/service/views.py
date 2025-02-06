c746e8829ddf6e8990e9866841f4ef5711b096f2
Anuj Choubey <114551422+Anujc07@users.noreply.github.com>
2025-02-07 00:15:28 +0530

    views.py

--- vehicle_service/service/views.py
+++ vehicle_service/service/views.py
@@ -8,19 +8,25 @@ from .serializers import ComponentSerializer, VehicleSerializer, IssueSerializer
 # this view return all components from the database.
 @api_view(['GET'])
 def get_components(request):
-    components = Component.objects.all()
-    serializer = ComponentSerializer(components, many=True)
-    return Response(serializer.data, status=status.HTTP_200_OK)
+    try:
+        components = Component.objects.all()
+        serializer = ComponentSerializer(components, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    except Exception as e:
+        return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 # this view for create a component
 @api_view(['POST'])
 def create_component(request):
-    print("===>", request.data)
-    serializer = ComponentSerializer(data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    try:
+        serializer = ComponentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    except Exception as e:
+        return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
 
 # this view return a component from the database.
 @api_view(['GET'])
@@ -31,8 +37,13 @@ def get_component(request, pk):
         return Response(serializer.data, status=status.HTTP_200_OK)
     except Component.DoesNotExist:
         return Response({'error': 'Component not found'}, status=status.HTTP_404_NOT_FOUND)
+    except Exception as e:
+        return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
 
 # this view for update specific component data.
+
+
 @api_view(['PUT'])
 def update_component(request, pk):
     try:
@@ -44,8 +55,12 @@ def update_component(request, pk):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     except Component.DoesNotExist:
         return Response({'error': 'Component not found'}, status=status.HTTP_404_NOT_FOUND)
+    except Exception as e:
+        return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
 
 # this view delete the component.
+
 @api_view(['DELETE'])
 def delete_component(request, pk):
     try:
@@ -54,25 +69,37 @@ def delete_component(request, pk):
         return Response({'message': 'Component deleted successfully'}, status=status.HTTP_204_NO_CONTENT)
     except Component.DoesNotExist:
         return Response({'error': 'Component not found'}, status=status.HTTP_404_NOT_FOUND)
+    except Exception as e:
+        return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 # ---- VEHICLE VIEWS ----
 # this view return all vechiles data from the database.
+
 @api_view(['GET'])
 def get_vehicles(request):
-    vehicles = Vehicle.objects.all()
-    serializer = VehicleSerializer(vehicles, many=True)
-    return Response(serializer.data, status=status.HTTP_200_OK)
+    try:
+        vehicles = Vehicle.objects.all()
+        serializer = VehicleSerializer(vehicles, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    except Exception as e:
+        return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
 
 # this view for add vehicle
+
 @api_view(['POST'])
 def create_vehicle(request):
-    serializer = VehicleSerializer(data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    try:
+        serializer = VehicleSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    except Exception as e:
+        return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 # this view return specific vehicle data
+
 @api_view(['GET'])
 def get_vehicle(request, pk):
     try:
@@ -81,8 +108,11 @@ def get_vehicle(request, pk):
         return Response(serializer.data, status=status.HTTP_200_OK)
     except Vehicle.DoesNotExist:
         return Response({'error': 'Vehicle not found'}, status=status.HTTP_404_NOT_FOUND)
+    except Exception as e:
+        return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 # this view for update the vehicle data
+
 @api_view(['PUT'])
 def update_vehicle(request, pk):
     try:
@@ -94,6 +124,9 @@ def update_vehicle(request, pk):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     except Vehicle.DoesNotExist:
         return Response({'error': 'Vehicle not found'}, status=status.HTTP_404_NOT_FOUND)
+    except Exception as e:
+        return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
 
 # this view for delete the vehicle
 @api_view(['DELETE'])
@@ -104,35 +137,50 @@ def delete_vehicle(request, pk):
         return Response({'message': 'Vehicle deleted successfully'}, status=status.HTTP_204_NO_CONTENT)
     except Vehicle.DoesNotExist:
         return Response({'error': 'Vehicle not found'}, status=status.HTTP_404_NOT_FOUND)
+    except Exception as e:
+        return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 # ---- ISSUE VIEWS ----
 # this view for get all the issues
+
 @api_view(['GET'])
 def get_issues(request):
-    issues = Issue.objects.all()
-    serializer = IssueSerializer(issues, many=True)
-    return Response(serializer.data, status=status.HTTP_200_OK)
-
-# this view for add the issue
-@api_view(['POST'])
-def create_issue(request):
-    serializer = IssueSerializer(data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-# this view for get a specific issue 
-@api_view(['GET'])
-def get_issue(request, pk):
-    issues = Issue.objects.filter(vehicle_id=pk)
-    if issues.exists():
+    try:
+        issues = Issue.objects.all()
         serializer = IssueSerializer(issues, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
-    else:
-        return Response({'error': 'Issue not found'}, status=status.HTTP_404_NOT_FOUND)
+    except Exception as e:
+        return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+# this view for add the issue
+
+@api_view(['POST'])
+def create_issue(request):
+    try:
+        serializer = IssueSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    except Exception as e:
+        return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+# this view for get a specific issue 
+
+@api_view(['GET'])
+def get_issue(request, pk):
+    try:
+        issues = Issue.objects.filter(vehicle_id=pk)
+        if issues.exists():
+            serializer = IssueSerializer(issues, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response({'error': 'Issue not found'}, status=status.HTTP_404_NOT_FOUND)
+    except Exception as e:
+        return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 # this view for update the issue
+
 @api_view(['PUT'])
 def update_issue(request, pk):
     try:
@@ -144,8 +192,12 @@ def update_issue(request, pk):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     except Issue.DoesNotExist:
         return Response({'error': 'Issue not found'}, status=status.HTTP_404_NOT_FOUND)
+    except Exception as e:
+        return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
 
 # this view for delete the issue
+
 @api_view(['DELETE'])
 def delete_issue(request, pk):
     try:
@@ -154,3 +206,5 @@ def delete_issue(request, pk):
         return Response({'message': 'Issue deleted successfully'}, status=status.HTTP_204_NO_CONTENT)
     except Issue.DoesNotExist:
         return Response({'error': 'Issue not found'}, status=status.HTTP_404_NOT_FOUND)
+    except Exception as e:
+        return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
